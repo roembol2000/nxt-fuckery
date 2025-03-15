5e8cc99d400b6e14c2138ea00e79190bfd867d3c
Roembol <roemerantoniepeters@gmail.com>
2025-03-15 22:20:34 +0100

    add basic connect nxt (not working)

--- src/index.js
+++ src/index.js
@@ -23,8 +23,45 @@ const GetDevices = () => {
   });
 };
 
+const ConnectToNXT = async () => {
+  try {
+    const device = await navigator.usb.requestDevice({
+      filters: [{ vendorId: 0x0694 }], // LEGO
+    });
+
+    await device.open();
+
+    if (device.configuration === null) {
+      await device.selectConfiguration(1);
+    }
+
+    await device.claimInterface(0);
+
+    const command = new Uint8Array([
+      0x02, // command length (2 bytes)
+      0x00, // command length continued
+      0x00, // direct command (with response)
+      0x0b, // get battery level
+    ]);
+
+    await device.transferOut(1, command);
+
+    const result = await device.transferIn(1, 5);
+
+    const batteryLevel = result.data.getUint16(3, true);
+
+    console.log(batteryLevel);
+
+    await device.close();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const btnRequestDevice = document.querySelector("#btn_request_device");
 const btnGetDevices = document.querySelector("#btn_get_devices");
+const btnConnectToNxt = document.querySelector("#btn_connect_to_nxt");
 
 btnRequestDevice.addEventListener("click", RequestDevice);
 btnGetDevices.addEventListener("click", GetDevices);
+btnConnectToNxt.addEventListener("click", ConnectToNXT);
